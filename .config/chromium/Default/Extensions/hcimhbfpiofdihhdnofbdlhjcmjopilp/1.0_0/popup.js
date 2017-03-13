6d396807f6da1ef9cc8da8cc06991b373103e654
Duncan R Hamill <duncanrhamill@googlemail.com>
2017-03-13 21:09:37 +0000

    removed chromium because i don't use it

--- .config/chromium/Default/Extensions/hcimhbfpiofdihhdnofbdlhjcmjopilp/1.0_0/popup.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('click',function(e){
-  if(e.target.href!==undefined){
-    chrome.tabs.create({url:e.target.href})
-  }
-})
--- .config/chromium/Default/Extensions/kaicbfmipfpfpjmlbpejaoaflfdnabnc/1.1.2_0/js/redirect.js
+++
@@ -1,5 +0,0 @@
-// Fallback when Chrome is not already running
-chrome.runtime.sendMessage({"action": "convertURL", "url": location.href},
-    function (response) {
-      	location.href = response;
-    });
